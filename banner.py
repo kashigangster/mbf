864f4cef56f4b036d70c258c0159487d92dace9e
kashigagster404 <68423787+kashigangster@users.noreply.github.com>
2020-10-31 18:26:14 +0500

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,22 @@
+C = "\033[94;m"
+G = "\033[92;m"
+r = "\033[0;m"
+P = "\033[93;m"
+M = "\033[91;m"
+def banner():
+    print(f"""
+{C}___  _______________ 
+{C}|  \/  || ___ \  ___| {r}[ {G}MULTI BRUTE FORCE {r}]
+{C}| .  . || |_/ / |_  {r}AUTHOR   {r}: KASHI-ARAIN
+{C}| |\/| || ___ \  _| {P}WhtsApp Num {r}:+923062045786
+{C}| |  | || |_/ / |   {C}GIFT {r}:PUBG/FREFIRE
+{C}\_|  |_/\____/\_|   {M}YOUTUBE  {r}:KASHI-TEACH
+            """.format(C,r,G))
+def menu():
+    print(f"{G}1{r} Crack From Friendlist")
+    print(f"{G}2{r} Crack From Likes On Post")
+    print(f"{G}3{r} Crack By Search Name")
+    print(f"{G}4{r} Crack From Group (Only Takes 100 User)")
+    print(f"{G}5{r} Crack From Friend Friendlist")
+    print(f"{G}6{r} Crack From Hashtag")
+    print(f"{G}7{r} Crack Re-check Result")
